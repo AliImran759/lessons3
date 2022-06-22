7f24de10f9ad33d30490910ee5db33abf90468bd
AliImran759 <aliimranbocenov@gmail.com>
2022-06-22 21:07:47 +0600

    first commit

---
+++ bin/lessons3.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+import 'package:lessons3/lessons3.dart' as lessons3;
+
+void main(List<String> arguments) {
+  int a1 = -3;
+
+  if (a1 <= 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+}
---
+++ test/lessons3_test.dart
@@ -0,0 +1,8 @@
+import 'package:lessons3/lessons3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
